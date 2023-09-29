94f8f63744a736d66314a4630c0e9043c84fd908
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-29 11:35:28 -0300

    adicionando arquivos html e js

---
+++ script.js
@@ -0,0 +1,25 @@
+// Variaveis com os dados e status, numero, e aray
+var nome = "Sergio";
+var idade = 20;
+var status = true;
+var valorProduto = 10.50;
+var listaTimes = ["Tempo1", "Tempo2", "Tempo3"];
+
+// Consoles ligados a variaveis
+console.log("Nome:", nome);
+console.log("Idade:", idade);
+console.log("Status:", status);
+console.log("Valor do Produto:", valorProduto);
+console.log("Lista de Times:", listaTimes);
+
+// variaveis das contas matematicas
+var soma = 5 + 3;
+var subtracao = 10 - 5;
+var multiplicacao = 2 * 4;
+var divisao = 20 / 4;
+
+// console ligado as contas matematicas
+console.log("Soma:", soma);
+console.log("Subtração:", subtracao);
+console.log("Multiplicação:", multiplicacao);
+console.log("Divisão:", divisao);
